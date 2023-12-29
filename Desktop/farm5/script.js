bbb0fb4a01a18120c3d3a586a691fac161156c6d
NASRALDI <nasraldin818@gmail.com>
2023-12-29 18:59:57 +0100

    goooo2

---
+++ Desktop/farm5/script.js
@@ -0,0 +1,8 @@
+function myFunction(){
+
+document.getElementById('foto').src='https://images.unsplash.com/photo-1444465146604-4fe67bfac6e8?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+}
+function myGoodFunction(){
+    document.getElementById('foto').src='https://images.unsplash.com/photo-1587004307582-ec2a58333438?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+window.alert("hello worlk")
+}
